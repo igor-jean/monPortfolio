9f4d7bacf1f1b053ec9a13bba54a7ea5ea269178
Igor Jean <igorjean79@gmail.com>
2024-07-28 15:25:36 +0200

    maj

--- contact.php
+++ contact.php
@@ -1,18 +1,31 @@
 <?php
-$nom = $_GET["nom"];
-$email = $_GET["email"];
-$tel = $_GET["telephone"];
-
+$nom = $_GET["nom"] ?? "";
 $prenom = $_GET["prenom"] ?? "";
+$email = $_GET["email"] ?? "";
+$tel = $_GET["telephone"] ?? "";
 $message = $_GET["message"] ?? "";
 $chk = $_GET["chk"] ?? "";
 
-$to      = 'igorjean79@gmail.com';
-$subject = 'Message depuis mon Portfolio';
-$contenu = `<h1>Message depuis mon Portfolio</h1><p>Nom: $nom<br>Prenom: $prenom<br>Email: $email<br>Téléphone: $tel<br>Message: $message</p>`;
-$headers = 'From: ' . $email . '' . "\r\n" .
-    'Reply-To: ' . $email . '' . "\r\n" .
-    "Content-Type: text/html; charset=UTF-8\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+if ($nom && $prenom && $email && $message && $chk) {
+    $to      = 'igorjean79@gmail.com';
+    $subject = 'Message depuis mon Portfolio';
+    $contenu = "<h1>Message depuis mon Portfolio</h1>
+                <p>Nom: $nom<br>
+                Prénom: $prenom<br>
+                Email: $email<br>
+                Téléphone: $tel<br>
+                Message: $message</p>";
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        "Content-Type: text/html; charset=UTF-8\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $contenu, $headers);
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        mail($to, $subject, $contenu, $headers);
+        echo "Votre message a été envoyé.";
+    } else {
+        echo "L'adresse email est invalide.";
+    }
+} else {
+    echo "Veuillez remplir tous les champs requis.";
+}
